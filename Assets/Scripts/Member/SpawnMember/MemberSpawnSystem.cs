9fc8b1be3c9d38b4b77591ef621a2a5a8d5de7b5
Fuzu <74610810+Fuzu-git@users.noreply.github.com>
2023-02-27 15:33:44 +0100

    Instantiated AI

--- Assets/Scripts/Member/SpawnMember/MemberSpawnSystem.cs
+++ Assets/Scripts/Member/SpawnMember/MemberSpawnSystem.cs
@@ -3,14 +3,16 @@ using System.Linq;
 using Lobby;
 using Mirror;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Member.SpawnMember
 {
     public class MemberSpawnSystem : NetworkBehaviour
     {
-        [SerializeField] private GameObject memberPrefab = null;
+        [SerializeField] private GameObject playerPrefab = null;
+        [SerializeField] private GameObject aiPrefab;
         private static List<Transform> _spawnPoints = new List<Transform>();
-        
+
         private int _nextIndex = 0;
 
         public static void AddSpawnPoint(Transform transform)
@@ -23,6 +25,9 @@ namespace Member.SpawnMember
 
         public override void OnStartServer()
         {
+            //Shuffle spawnpoints. 
+            Shuffle(_spawnPoints);
+            
             NetworkManagerLobby.OnServerReadied += SpawnPlayer;
         }
 
@@ -33,17 +38,39 @@ namespace Member.SpawnMember
         public void SpawnPlayer(NetworkConnection conn)
         {
             Transform spawnPoint = _spawnPoints.ElementAtOrDefault(_nextIndex);
-            
+
             if (spawnPoint == null)
             {
                 Debug.LogError("Missing spawn point for player " + _nextIndex);
-                return; 
+                return;
             }
 
-            GameObject playerInstance = Instantiate(memberPrefab, _spawnPoints[_nextIndex].position, _spawnPoints[_nextIndex].rotation);
+            GameObject playerInstance = Instantiate(playerPrefab, _spawnPoints[_nextIndex].position,
+                _spawnPoints[_nextIndex].rotation);
             NetworkServer.Spawn(playerInstance, conn);
 
+            SpawnAI(_nextIndex);
+            
             _nextIndex++;
+        }
+
+        public void SpawnAI(int nextNextIndex)
+        {
+            nextNextIndex++; 
+            GameObject aiInstance = Instantiate(aiPrefab, _spawnPoints[nextNextIndex].position, _spawnPoints[nextNextIndex].rotation);
+            NetworkServer.Spawn(aiInstance);
+            _spawnPoints.RemoveAt(nextNextIndex);
+        }
+
+        public void Shuffle(List<Transform> list)
+        {
+            int n = list.Count;
+            while (n > 1)
+            {
+                n--;
+                int k = Random.Range(0, n + 1);
+                (list[k], list[n]) = (list[n], list[k]);
+            }
         }
     }
 }
